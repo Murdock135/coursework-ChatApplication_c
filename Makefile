60a8f66e105fb8a25eb5cce49e92ebaa105763ff
Zayan <zayan.qazi.mail@gmail.com>
2026-02-20 09:29:54 -0600

    copied code from youtube tutorial https://www.youtube.com/watch?v=LtXEMwSG5-8&t=1192s

---
+++ Makefile
@@ -0,0 +1,20 @@
+# Example Makefile structure for a C project:
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Iinclude
+
+SRC = $(wildcard src/*.c)
+OBJ = $(SRC:src/%.c=build/%.o)
+TARGET = build/myapp
+
+$(TARGET): $(OBJ)
+	$(CC) $(OBJ) -o $@
+
+build/%.o: src/%.c | build
+	$(CC) $(CFLAGS) -c $< -o $@
+
+build:
+	mkdir -p build
+
+clean:
+	rm -rf build
---
+++ src/client.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    // create a socket
+    int network_socket;
+    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specify an address for the socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    if (connection_status != 0) printf("Connection Failed");
+
+    return 0;
+}
