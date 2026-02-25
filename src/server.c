eec4b82d25945d53f807829f50f28353a3674712
Zayan <zayan.qazi.mail@gmail.com>
2026-02-25 09:13:07 -0600

    server.c

---
+++ src/server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netdb.h>
+
+int main(void) {
+    struct addrinfo hints;
+    struct addrinfo * res; // will point to the results
+    
+    memset(&hints, 0, sizeof hints);
+    return 0;
+}
