c70006fc25649ec3ae01437b1f172f7cae6270fa
Zayan <zayan.qazi.mail@gmail.com>
2026-02-25 10:42:59 -0600

    basic getaddrinfo usage

--- src/server.c
+++ src/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,8 +9,15 @@
 
 int main(void) {
     struct addrinfo hints;
-    struct addrinfo * res; // will point to the results
+    struct addrinfo * head; // will point to the head of addrinfo-linked-list
+    int status;
     
-    memset(&hints, 0, sizeof hints);
+    memset(&hints, 0, sizeof hints); // Ensure everything is empty (0)
+    status = getaddrinfo(NULL, "3490", &hints, &head); // returns a status
+    if (status != 0 ) {
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+    else printf("Success!");
     return 0;
 }
